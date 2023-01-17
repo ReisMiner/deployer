7db8d41a742f1a80508fce04e3ca5c5161709973
ReisMiner <r1miner@outlook.com>
2023-01-17 15:06:30 +0100

    smol fix

--- src/main/java/cc/ramon/Bot/Commands/TestEmbed.java
+++ src/main/java/cc/ramon/Bot/Commands/TestEmbed.java
@@ -21,20 +21,21 @@ public class TestEmbed extends ExtendedCommand {
         EmbedBuilder embedBuilder = new EmbedBuilder();
         String repo = "deployer";
         String job = "build";
+        String success = "success";
         if (event.getOption("repo") != null)
             repo = event.getOption("repo").getAsString();
         if (event.getOption("job") != null)
             job = event.getOption("job").getAsString();
 
 
-        embedBuilder.setTitle("[" + repo + "] " + job + " success on master", "https://github.com/ReisMiner/deployer/actions/runs/3738591536/jobs/6344829009");
-
         OptionMapping successOption = event.getOption("success");
-        if (successOption != null && successOption.getAsBoolean())
+        if (successOption != null && successOption.getAsBoolean()) {
             embedBuilder.setColor(new Color(0, 152, 0));
-        else
+        } else {
             embedBuilder.setColor(new Color(252, 43, 43));
+        }
 
+        embedBuilder.setTitle("[" + repo + "] " + job + " " + success + " on master", "https://github.com/ReisMiner/deployer/actions/runs/3738591536/jobs/6344829009");
         event.replyEmbeds(embedBuilder.build()).queue();
     }
 }
